471a67bfd880f39c17b186a7b35c4cdccecfd187
covid19aj-hue <covid19.aj@gmail.com>
2026-04-05 11:09:11 +0800

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,22 @@
+#diff = 0
+#num1 = 100
+#num2 = 95
+#diff = abs(num2 - num1)
+#print("Difference between", num1, "and", num2, "is:", diff)
+
+
+def my_diff(num1, num2):
+  diff = abs(num2 - num1)
+  print("Difference between", num1, "and", num2, "is:", diff)
+
+my_diff(100, 95)
+my_diff(85, 20)
+my_diff(8888, 3000)
+
+def my_squre(num):
+  squre = num * num
+  print("Squre of", num, "is:", squre)
+
+my_squre(10)
+my_squre(20)
+my_squre(30)
